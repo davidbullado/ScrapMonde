90f924189133ee1cbd6fba8163360f5ab3dd58c9
David Bullado <david.bullado@gmail.com>
2018-10-10 11:07:18 +0000

    Add --user to installrequirment

--- installrequierment.sh
+++ installrequierment.sh
@@ -4,4 +4,4 @@ pip3 install --upgrade pip
 pip3 install requests BeautifulSoup4
 pip3 install flask flask-jsonpify flask-sqlalchemy flask-restful
 pip3 install -U flask-cors
-pip3 install dateparser
+pip3 install --user dateparser

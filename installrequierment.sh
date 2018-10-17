e50c9c3a3474b298cb99b08b67e5911ec9c8bb34
David Bullado <david.bullado@gmail.com>
2018-10-17 08:09:47 +0000

    instrallrequirment : add --user

--- installrequierment.sh
+++ installrequierment.sh
@@ -5,4 +5,4 @@ pip3 install requests BeautifulSoup4
 pip3 install flask flask-jsonpify flask-sqlalchemy flask-restful
 pip3 install -U flask-cors
 pip3 install --user dateparser
-pip3 install coloredlogs
+pip3 install --user coloredlogs
